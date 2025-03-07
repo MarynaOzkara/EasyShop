a7c415ce116e5a231ce1e8a8487c5e192eace676
MarynaOzkara <ozkaramaryna@gmail.com>
2025-03-07 21:08:10 +0200

    add gender collections section

--- frontend/src/components/Common/Footer.jsx
+++ frontend/src/components/Common/Footer.jsx
@@ -7,8 +7,8 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="border-t py-12">
-      <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-8 px-4 lg:px-0">
+    <footer className="border-t py-12 px-4">
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-8  lg:px-0">
         <div>
           <h3 className="text-lg text-gray-800 mb-4">Newsletter</h3>
           <p className="text-gray-500 mb-4">
