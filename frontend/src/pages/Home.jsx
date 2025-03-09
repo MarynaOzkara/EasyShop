d92d2e7ba121b1ac0a605a77be647458fad29a3e
MarynaOzkara <ozkaramaryna@gmail.com>
2025-03-09 18:21:57 +0200

    add BestSeller section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../components/Layout/Hero";
 import GenderCollection from "../components/Products/GenderCollection";
 import NewArrivals from "../components/Products/NewArrivals";
+import BestSeller from "../components/Products/bestSeller";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <GenderCollection />
       <NewArrivals />
+      <BestSeller />
     </div>
   );
 };
