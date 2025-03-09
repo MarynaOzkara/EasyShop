b5242e0ea4df224ab16096132e3c8c7355282733
MarynaOzkara <ozkaramaryna@gmail.com>
2025-03-09 19:45:54 +0200

    fixing BestSeller

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Hero from "../components/Layout/Hero";
 import GenderCollection from "../components/Products/GenderCollection";
 import NewArrivals from "../components/Products/NewArrivals";
-import BestSeller from "../components/Products/bestSeller";
+import BestSeller from "../components/Products/BestSeller";
 
 const Home = () => {
   return (
