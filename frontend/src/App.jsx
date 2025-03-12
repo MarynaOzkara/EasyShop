3c114767037f8e977026e9be12bc7b13efe54e03
MarynaOzkara <ozkaramaryna@gmail.com>
2025-03-12 20:22:32 +0200

    finish ui home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,12 @@ import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+      }}
+    >
       <Toaster position="top-right" />
       <Routes>
         <Route path="/" element={<UserLayout />}>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,9 @@ import Hero from "../components/Layout/Hero";
 import GenderCollection from "../components/Products/GenderCollection";
 import NewArrivals from "../components/Products/NewArrivals";
 import BestSeller from "../components/Products/BestSeller";
+import WomenTopWear from "../components/Products/WomenTopWear";
+import FeaturedCollection from "../components/Products/FeaturedCollection";
+import FeaturesSection from "../components/Products/FeaturesSection";
 
 const Home = () => {
   return (
@@ -11,6 +14,9 @@ const Home = () => {
       <GenderCollection />
       <NewArrivals />
       <BestSeller />
+      <WomenTopWear />
+      <FeaturedCollection />
+      <FeaturesSection />
     </div>
   );
 };
