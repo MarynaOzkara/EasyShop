6a14de43e3be9266dfd2d5b6c662daf9d1ec65b8
MarynaOzkara <ozkaramaryna@gmail.com>
2025-03-22 20:53:55 +0200

    add Order Confirmation page

--- frontend/src/components/Layout/CartDrawer.jsx
+++ frontend/src/components/Layout/CartDrawer.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 const CartDrawer = ({ openDrawer, toggleCartDrawer }) => {
   const navigate = useNavigate();
   const handleCheckout = () => {
+    toggleCartDrawer();
     navigate("/checkout");
   };
   return (
