712ec00fe9e685a7b37f3e1760d1e27b4fc61bd6
MarynaOzkara <ozkaramaryna@gmail.com>
2025-05-13 21:20:59 +0300

    add order and checkout redux

---
+++ frontend/src/redux/slices/adminSlice.js
@@ -0,0 +1,14 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const fetchUsers = createAsyncThunk("admin/fetchUsers", async () => {
+  const response = await axios.get(
+    `${import.meta.env.VITE_BACKEND_URL}/api/admin/users`,
+    {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+      },
+    }
+  );
+  return response.data;
+});
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import productsReducer from "./slices/productsSlice";
+import cartReducer from "./slices/cartSlice";
+import checkoutReducer from "./slices/checkoutSlice";
+import orderReducer from "./slices/orderSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     products: productsReducer,
+    cart: cartReducer,
+    checkout: checkoutReducer,
+    orders: orderReducer,
   },
 });
 export default store;
