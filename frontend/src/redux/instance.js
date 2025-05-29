cb2e7a1be0efba85da634be4db2fb71634ff5ae8
MarynaOzkara <ozkaramaryna@gmail.com>
2025-05-29 22:11:36 +0300

    change backend deploed url

--- frontend/src/redux/instance.js
+++ frontend/src/redux/instance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const backend = axios.create({
-  //   baseURL: "https://easyshop-gh3r.onrender.com",
-  baseURL: "http://localhost:9000",
+  baseURL: "https://easyshop-tic5.onrender.com",
+  // baseURL: "http://localhost:9000",
 });
