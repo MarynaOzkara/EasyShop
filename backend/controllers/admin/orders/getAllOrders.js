c150eed4015611891b4103172d3a6971944b2105
MarynaOzkara <ozkaramaryna@gmail.com>
2025-05-29 22:19:34 +0300

    fix admin orders sort

--- backend/controllers/admin/orders/getAllOrders.js
+++ backend/controllers/admin/orders/getAllOrders.js
@@ -2,7 +2,9 @@ const { ctrWrapper } = require("../../../decorators");
 const Order = require("../../../models/order");
 
 const getAllOrders = async (req, res) => {
-  const orders = await Order.find({}).populate("user", "name email");
+  const orders = await Order.find({})
+    .sort({ createdAt: -1 })
+    .populate("user", "name email");
   const total = await Order.countDocuments();
   res.json({ total, orders });
 };
--- backend/controllers/admin/products/getAllProducts.js
+++ backend/controllers/admin/products/getAllProducts.js
@@ -2,7 +2,7 @@ const { ctrWrapper } = require("../../../decorators");
 const Product = require("../../../models/product");
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find({});
+  const products = await Product.find({}).sort({ createdAt: -1 });
   const total = await Product.countDocuments();
   res.json({ total, products });
 };
